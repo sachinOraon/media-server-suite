17bfe28dcede81412aa66999f1542ded1412a741
Sachin Oraon <sachinoraon55@gmail.com>
2022-07-08 14:07:59 +0530

    Create Container.py

---
+++ piboard/Container.py
@@ -0,0 +1,8 @@
+class Container:
+    def __init__(self):
+        self.portainer = "portainer"
+        self.qbit = "qbittorrent"
+        self.filebrowser = "filebrowser"
+        self.h5ai = "h5ai"
+
+container = Container()
